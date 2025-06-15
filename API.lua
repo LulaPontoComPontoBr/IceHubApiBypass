5da5fa6dc4ac702b5179e0eb5bc7941ef67977bd
LulaPontoComPontoBr <analcarolaine2323@gmail.com>
2025-06-15 19:08:55 -0300

    Create API.lua

---
+++ API.lua
@@ -0,0 +1,54 @@
+return {
+	_credits = {
+		_icy = "Bypassadoporlz",
+		_hexr = "Bypassadoporlz",
+		_xred = "makotoshin234",
+		_img = "9329990575"
+	},
+
+	_event_connection = {
+		_HALLOWEEN_CONNECTION = { _halloween = false },
+		_CHRISTMAS_CONNECTION = { _christmas = false }
+	},
+
+	_ADOPT_ME_API = {
+		_main_connected = false,
+		_adopt_me_halloween = {
+			_minimage_1 = nil,
+			_minigame_2 = nil,
+			_wrnt = true,
+			_wrnms = "Many exploiters get banned during the Halloween event, be sure to use your alt account."
+		},
+		_adopt_me_christmas = {
+			_minimage_1 = nil,
+			_minigame_2 = nil,
+			_wrnt = true,
+			_wrnms = "Many exploiters get banned during the Christmas event, be sure to use your alt account."
+		}
+	},
+
+	_rbxev_mode = {
+		_normal_mode = {
+			_ttl = Color3.fromRGB(255, 255, 255),
+			_bps = Color3.fromRGB(0, 255, 255),
+			_lblt = Color3.fromRGB(0, 255, 255),
+			_te = ""
+		},
+		_halloween_mode = {
+			_ttl = Color3.fromRGB(255, 85, 0),
+			_bps = Color3.fromRGB(255, 85, 0),
+			_lblt = Color3.fromRGB(255, 85, 0),
+			_te = "🎃"
+		},
+		_christmas_mode = {
+			_ttl = Color3.fromRGB(255, 0, 0),
+			_bps = Color3.fromRGB(255, 0, 0),
+			_lblt = Color3.fromRGB(255, 0, 0),
+			_te = "🎅"
+		}
+	},
+
+	_discord = {
+		_nwl = "2H5j3cNEgJ"
+	}
+}
